c9163c2d846c4c98293974dd8a7885b8d42f01b3
Paula Kurniawan <paula.kurn@gmail.com>
2020-02-04 18:09:31 +1100

    Implemented the api controllers, all working perfectly

---
+++ WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Models.DataManagers;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private AccountManager _repo;
+
+        public AccountsController(AccountManager repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/Accounts
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return _repo.GetAll();
+        }
+
+        // GET: api/Accounts/5
+        [HttpGet("{id}")]
+        public Account Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        // POST: api/Accounts
+        [HttpPost]
+        public void Post([FromBody] Account customer)
+        {
+            _repo.Add(customer);
+        }
+
+        // PUT: api/Accounts/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Account customer)
+        {
+            _repo.Update(id, customer);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repo.Delete(id);
+        }
+    }
+}
