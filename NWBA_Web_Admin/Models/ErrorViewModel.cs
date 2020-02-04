c7d61d6aa020821689934913eba6de6f2e67a8de
Paula Kurniawan <paula.kurn@gmail.com>
2020-02-04 15:55:24 +1100

    A3 base solutions and projects made

---
+++ NWBA_Web_Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NWBA_Web_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
