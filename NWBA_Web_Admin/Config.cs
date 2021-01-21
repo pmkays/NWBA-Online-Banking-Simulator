4f8ed7fcfc048c40b053433fc4e111dba7a0baf4
Paula Kurniawan <paula.kurn@gmail.com>
2021-01-21 14:51:30 +1100

    Use env variables for webapi url from webadmin

---
+++ NWBA_Web_Admin/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NWBA_Web_Admin
+{
+    public class Config
+    {
+    }
+}
--- NWBA_Web_Admin/Startup.cs
+++ NWBA_Web_Admin/Startup.cs
@@ -16,9 +16,12 @@ namespace NWBA_Web_Admin
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            StaticConfig = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public static IConfiguration StaticConfig { get; private set; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
